16c34bd7adf36c3b0ff28f16b583bf1d877a18ea
Chuqdi <103970327+Chuqdi@users.noreply.github.com>
2023-01-10 20:57:32 -0800

    models_and_database

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField()
+    is_published = models.BooleanField(default=True)
+    date_created = models.DateField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.name
