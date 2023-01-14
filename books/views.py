d23dd3867fc851869a192ba64aed8d374ab71433
Chuqdi <103970327+Chuqdi@users.noreply.github.com>
2023-01-14 12:01:33 -0800

    django_forms_request_post_handling

--- books/views.py
+++ books/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from .models import Book
+from .forms import BookForm
+from django.http import HttpResponse
 
-
-def book(request):
-    bookName ="Learning Django"
-   
+def index(request):
     return render(
         request,
         "index.html",
-        context={
-        }
+
+    )
 
 
+def create_book(request):
+    if request.method == "POST":
+        
+        bookForm = BookForm(request.POST)
+        if bookForm.is_valid():
+            bookForm.save()
+            return HttpResponse("Book was creates successfully")
+    else:
+        bookForm = BookForm()
+    return render(request, "create_book.html",
+    {
+        "form":bookForm
+    }
     )
 
--- complete_django_beginner/urls.py
+++ complete_django_beginner/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from books.views import book
+from books.views import index, create_book
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("book", book, name="book"),
+    path("", index, name="index"),
+    path("create_book", create_book, name="create_book")
 
 ]
 
