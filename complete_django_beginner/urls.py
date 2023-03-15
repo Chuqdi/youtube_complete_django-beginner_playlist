7e7ea7a15f6d98cfa41df27631472eae27d97419
Chuqdi <103970327+Chuqdi@users.noreply.github.com>
2023-03-15 07:00:42 -0700

    user_login

--- complete_django_beginner/urls.py
+++ complete_django_beginner/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from books.views import index,create_book, list_books
 from users.views import sign_up
-
+from django.contrib.auth.views import LoginView
 
 
 
@@ -12,7 +12,8 @@ urlpatterns = [
     path("", index, name="index"),
     path("create_book", create_book, name="create_book"),
     path("list_books", list_books, name="list_books"),
-    path("sign_up", sign_up, name="sign_up")
+    path("sign_up", sign_up, name="sign_up"),
+    path("login", LoginView.as_view(template_name="login.html"), name="login")
 
 ]
 
