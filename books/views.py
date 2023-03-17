9be9e920753e1a682c2b76691f6988c29b9b5ba5
Chuqdi <103970327+Chuqdi@users.noreply.github.com>
2023-03-17 05:52:03 -0700

    update_delete_with_authentication_and_permission

--- books/views.py
+++ books/views.py
@@ -5,6 +5,36 @@ from .forms import BookForm
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.generic import UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+
+
+
+
+class DeleteBook(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+    model = Book
+    template_name ="delete.html"
+    success_url ="/list_books"
+
+    def test_func(self):
+        ##HOW TO GET THE QUERYSET INSTANCE
+        ##self.get_object()
+        
+        ##HOW TO GET THE LOGGED IN USER
+        #self.request.user
+
+        return self.get_object().creator == self.request.user
+        
+
+
+
+class UpdateBook(LoginRequiredMixin,UpdateView):
+    model = Book
+    template_name ="update.html"
+    fields = ['name', 'description']
+    success_url ="/list_books"   
 
 
 
@@ -12,6 +42,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def list_books(request):
     books = Book.objects.filter(creator=request.user).order_by("-id")
+    print(books)
     return render(request, "list_books.html",{"books":books})
 
 
@@ -28,7 +59,9 @@ def create_book(request):
 
         if form.is_valid():
             form = form.save(commit=False)
+            form.creator = request.user
             messages.success(request, "Book was created successfully")
+            form.save()
 
             
             return redirect("list_books")
--- complete_django_beginner/urls.py
+++ complete_django_beginner/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from books.views import index,create_book, list_books
+from books.views import index,create_book, list_books, DeleteBook, UpdateBook
 from users.views import sign_up
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -11,6 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name="index"),
     path("create_book", create_book, name="create_book"),
+    path("delete_book/<int:pk>", DeleteBook.as_view(), name="delete_book"),
+    path("update/<int:pk>", UpdateBook.as_view(), name="update_book"),
     path("list_books", list_books, name="list_books"),
     path("sign_up", sign_up, name="sign_up"),
 
